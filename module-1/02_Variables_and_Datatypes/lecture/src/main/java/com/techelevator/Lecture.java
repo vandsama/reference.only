bcda568e08e8a151cb0edb7ef1041e6722b78941
Samantha Vanderburgh <vandsama@gmail.com>
2022-09-13 14:05:48 -0400

    Submitting Week 1 Day 1 exercise

--- module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
@@ -3,130 +3,150 @@ package com.techelevator;
 public class Lecture {
 
 	public static void main(String[] args) {
-		
+
+
 		/*
 		1. Create a variable to hold an int and call it numberOfExercises.
 			Then set it to 26.
 		*/
-
-		//System.out.println(numberOfExercises);
+		int numberOfExercises = 26;
+		System.out.println(numberOfExercises);
 
 		/*
 		2. Create a variable to hold a double and call it half.
 			Set it to 0.5.
 		*/
-
-		//System.out.println(half);
+		double half = 0.5;
+		System.out.println(half);
 
 		/*
 		3. Create a variable to hold a String and call it name.
 			Set it to "TechElevator".
 		*/
-
-		//System.out.println(name);
+		String name = "tech elevator";
+		System.out.println(name);
 
 		/*
 		4. Create a variable called seasonsOfFirefly and set it to 1.
 		*/
-
-		//System.out.println(seasonsOfFirefly);
+		int seasonsOfFirefly = 1;
+		System.out.println(seasonsOfFirefly);
 
 		/*
 		5. Create a variable called myFavoriteLanguage and set it to "Java".
 		*/
-
-		//System.out.println(myFavoriteLanguage);
+		String myFavoriteLanguage = "Java";
+		System.out.println(myFavoriteLanguage);
 
 		/*
 		6. Create a variable called pi and set it to 3.1416.
 		*/
-
-		//System.out.println(pi);
+		double pi = 3.1416;
+		System.out.println(pi);
 
 		/*
 		7. Create and set a variable that holds your name.
 		*/
-
+		String myName = "samantha";
+		System.out.println(myName);
 		/*
 		8. Create and set a variable that holds the number of buttons on your mouse.
 		*/
-
+		int numberOfButtonsOnMouse = 5;
 		/*
 		9. Create and set a variable that holds the percentage of battery left on
 		your phone.
 		*/
-
+		int percentOfBatteryLeft = 73;
 		/*
 		10. Create an int variable that holds the difference between 121 and 27.
 		*/
-
+		int difference = 121 - 27;
+		System.out.println(difference);
 		/*
 		11. Create a double that holds the addition of 12.3 and 32.1.
 		*/
-
+		double sum = 12.3 + 32.1;
+		System.out.println(sum);
 		/*
 		12. Create a String that holds your full name.
 		*/
-
+		String fullName = "samantha vanderburgh";
+		System.out.println(fullName);
 		/*
 		13. Create a String that holds the word "Hello, " concatenated onto your
 		name from above.
 		*/
-
+		String greeting = "Hello " + fullName;
+		System.out.println(greeting);
 		/*
 		14. Add a " Esquire" onto the end of your full name and save it back to
 		the same variable.
 		*/
-
+		fullName = fullName + ", esquire";
+		System.out.println(fullName);
 		/*
 		15. Now do the same as exercise 14, but use the += operator.
 		*/
-
+		fullName += ", esquire";
+		System.out.println(fullName);
 		/*
 		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
 		*/
-
+		String saw = "Saw" + 2;
+		System.out.println(saw);
 		/*
 		17. Add a 0 onto the end of the variable from exercise 16.
 		*/
-
+		saw += 0;
+		System.out.println(saw);
 		/*
 		18. What is 4.4 divided by 2.2?
 		*/
-
+		System.out.println(4.4/2.2);
 		/*
 		19. What is 5.4 divided by 2?
 		*/
-
+		System.out.println(5.4/2);
 		/*
 		20. What is 5 divided by 2?
 		*/
-
+		System.out.println(5/2);
 		/*
 		21. What is 5.0 divided by 2?
 		*/
-
+		System.out.println(5.0/2);
 		/*
 		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
 		*/
-
+		System.out.println(66.6/100);
 		/*
 		23. If I divide 5 by 2, what's my remainder?
 		*/
-
+		System.out.println(5%2);
 		/*
 		24. What is 1,000,000,000 * 3?
 		*/
-
+		long bigNumber = 1_000_000_000L * 3;
+		System.out.println(bigNumber);
 		/*
 		25. Create a variable that holds a boolean called isDoneWithExercises and
 		set it to false.
 		*/
-
+		boolean isDoneWithExercises = false;
 		/*
 		26. Now set isDoneWithExercise to true.
 		*/
-		
-	}
+		isDoneWithExercises = true;
 
+		int x = (int)10L;
+
+		final int hoursInYear = 2080;
+		final double rateFactor = 1.07;
+		final double hourlyPay = 15.0;
+		final int PAY_PERIODS = 26;
+		double r = hoursInYear * hourlyPay * rateFactor / PAY_PERIODS;
+
+		final double PI = 3.14;
+		final float PI = 3.14F;
 }
