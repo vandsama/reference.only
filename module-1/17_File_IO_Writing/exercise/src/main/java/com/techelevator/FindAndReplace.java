faa0226b0fc2a15d7e1040a8e2e8c64235b9081e
Samantha Vanderburgh <vandsama@gmail.com>
2022-10-09 22:41:02 -0400

    Completed HW 17 File IO Writing

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,44 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourceFileName = userInput.nextLine();
+        File searchFile = new File(sourceFileName);
+
+        System.out.println("What is the destination file?");
+        String destinationFileName = userInput.nextLine();
+        File destinationFile = new File(destinationFileName);
+
+        String newLineOfText;
+
+        try (Scanner fileInput = new Scanner(searchFile); PrintWriter writer = new PrintWriter(destinationFile)) {
+            while (fileInput.hasNextLine()) {
+                String lineOfText = fileInput.nextLine();
+
+                newLineOfText = lineOfText.replaceAll(searchWord,replacementWord);
+
+                writer.println(newLineOfText);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("The file was not found: " + searchFile.getAbsolutePath());
+            return;
+        }
+
 
     }
 
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,48 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What is the destination file?");
+		String destinationFileName = userInput.nextLine();
+		File destinationFile = new File(destinationFileName);
 
+	try (PrintWriter writer = new PrintWriter(destinationFile)) {
+		for (int i = 1; i <= 300; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				writer.println("FizzBuzz");
+			}
+			else if (i % 3 == 0){
+				writer.println("Fizz");
+			}
+			else if (i % 5 == 0) {
+				writer.println("Buzz");
+			}
+			else {
+				writer.println(i);
+			}
+		}
+//		If the number is divisible by 3, print Fizz.
+
+//		If the number is divisible by 5, print Buzz.
+
+//		If the number is divisible by 3 and 5, print FizzBuzz.
+
+//		Otherwise, print the number.
+
+
+
+	} catch (
+	FileNotFoundException e) {
+		System.out.println("The file was not found: " + destinationFile.getAbsolutePath());
+		return;
+	}
 	}
 
 }
