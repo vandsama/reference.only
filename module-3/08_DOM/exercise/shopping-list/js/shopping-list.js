046fd48c5955e768ef2fb0abf32880500de23e69
Samantha Vanderburgh <vandsama@gmail.com>
2022-11-17 15:03:38 -0500

    Submitting HW 08 DOM

--- module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
@@ -1,23 +1,84 @@
 // add pageTitle
 
+pageTitle = 'My Shopping List';
+
 // add groceries
+
+groceries = [{
+  id: 1,
+  item: 'firstItem'
+},
+{
+  id: 2,
+  item: 'secondItem'
+},
+{
+  id: 3,
+  item: 'thirdItem'
+},
+{
+  id: 4,
+  item: 'fourthItem'
+},
+{
+  id: 5,
+  item: 'fifthItem'
+},
+{
+  id: 6,
+  item: 'sixthItem'
+},
+{
+  id: 7,
+  item: 'seventhItem'
+},
+{
+  id: 8,
+  item: 'eighthItem'
+},
+{
+  id: 9,
+  item: 'ninthItem'
+},
+{
+  id: 10,
+  item: 'tenthItem'
+}
+];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const heading = document.getElementById('title');
+  heading.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const groceryList = document.getElementById('groceries');
+  for (grocery of groceries) {
+    const li = document.createElement('li');
+    li.innerText = grocery.item;
+    groceryList.appendChild(li);
+  }
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  const allGroceryItems = document.querySelectorAll('li');
+  allGroceryItems.forEach((g) => {
+    g.classList.add('completed')
+  })
+
+
+}
 
 setPageTitle();
 
