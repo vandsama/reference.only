20b7c7226ec1789db5f7888724ffe324dada5e52
Samantha Vanderburgh <vandsama@gmail.com>
2022-11-27 14:11:24 -0500

    Passed 8/15 tests - Submitting late and incomplete homework 11 Intro to Vue

--- module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -1,22 +1,30 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
         <td>
-          <select id="statusFilter">
+          <input type="text" id="firstNameFilter" v-model="search.firstName" />
+        </td>
+        <td>
+          <input type="text" id="lastNameFilter" v-model="search.lastName" />
+        </td>
+        <td>
+          <input type="text" id="usernameFilter" v-model="search.username" />
+        </td>
+        <td>
+          <input type="text" id="emailFilter" v-model="search.emailAddress" />
+        </td>
+        <td>
+          <select id="statusFilter" v-model="search.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
@@ -24,35 +32,96 @@
         </td>
       </tr>
       <!-- user listing goes here -->
+      <tr v-for="(user, index) in filteredList" v-bind:key="index" v-bind:class="{inactive:user.status==='Inactive'}">
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
-
 <script>
 export default {
-  name: 'user-list',
+  name: "user-list",
   data() {
     return {
       users: [
-        { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
-        { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
-        { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Inactive' },
-        { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
-        { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
-        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Inactive' }
-      ]
-    }
-  }
-}
+        {
+          firstName: "John",
+          lastName: "Smith",
+          username: "jsmith",
+          emailAddress: "jsmith@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Anna",
+          lastName: "Bell",
+          username: "abell",
+          emailAddress: "abell@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "George",
+          lastName: "Best",
+          username: "gbest",
+          emailAddress: "gbest@gmail.com",
+          status: "Inactive",
+        },
+        {
+          firstName: "Ben",
+          lastName: "Carter",
+          username: "bcarter",
+          emailAddress: "bcarter@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Katie",
+          lastName: "Jackson",
+          username: "kjackson",
+          emailAddress: "kjackson@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "Mark",
+          lastName: "Smith",
+          username: "msmith",
+          emailAddress: "msmith@foo.com",
+          status: "Inactive",
+        },
+      ],
+      search: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "",
+      },
+    };
+  },
+  computed: {
+    filteredList() {
+      return this.users.search((user) => {
+        const containsFirstName = user.firstName.toLowerCase().includes(this.search.firstName.toLowerCase());
+        const containsLastName = user.lastName.toLowerCase().includes(this.search.lastName.toLowerCase());
+        const containsUsername = user.username.toLowerCase().includes(this.search.username.toLowerCase());
+        const containsEmailAddress = user.emailAddress.toLowerCase().includes(this.search.emailAddress.toLowerCase());
+        const containsStatus = user.status.includes(this.search.status);
+        return containsFirstName && containsLastName && containsUsername && containsEmailAddress && containsStatus;
+      });
+    },
+  },
+};
 </script>
-
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
@@ -60,7 +129,8 @@ td {
 tr.disabled {
   color: red;
 }
-input, select {
+input,
+select {
   font-size: 16px;
 }
 </style>
